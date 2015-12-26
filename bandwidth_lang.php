e09f15b69fd29adec8ba0c0b90d6bd4c852907b4
liwentao0705 <liwentao0705@outlook.com>
2015-12-26 21:41:26 +0800

    Update bandwidth_lang.php

--- bandwidth_lang.php
+++ bandwidth_lang.php
@@ -26,14 +26,14 @@ if(!defined('VIRTUALIZOR')){
 
 }
 
-$l['<title>'] = 'Bandwidth Usage';
-$l['heading'] = 'Bandwidth';
-$l['bandwidthinfo'] = 'Bandwidth Information';
-$l['Total_Bandwidth'] = 'Total Bandwidth :';
-$l['Bandwidth_utilised'] = 'Utilised : ';
-$l['percent_utilised'] = '% utilized : ';
-$l['graphheader'] = 'Bandwidth Utilization';
-$l['unlimited'] = 'Unlimited';
-$l['network_speed'] = 'Network Speed';
+$l['<title>'] = '流量使用情况';
+$l['heading'] = '流量';
+$l['bandwidthinfo'] = '流量信息';
+$l['Total_Bandwidth'] = '总流量 :';
+$l['Bandwidth_utilised'] = '已用流量 : ';
+$l['percent_utilised'] = '% 已用 : ';
+$l['graphheader'] = '占比';
+$l['unlimited'] = '无限';
+$l['network_speed'] = '网速';
 
 ?>
